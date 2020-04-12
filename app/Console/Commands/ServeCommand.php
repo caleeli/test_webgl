e836ad863a098894dd62dbe06004912eff9c9209
David Callizaya <davidcallizaya@gmail.com>
2020-04-12 10:10:07 -0400

    Validate initial port

--- app/Console/Commands/ServeCommand.php
+++ app/Console/Commands/ServeCommand.php
@@ -18,9 +18,9 @@ class ServeCommand extends Command
     {
         $config = parse_url(config('app.url'));
         return [
-            ['host', null, InputOption::VALUE_OPTIONAL, 'The host address to serve the application on', $config['host']],
+            ['host', null, InputOption::VALUE_OPTIONAL, 'The host address to serve the application on', $config['host'] ?? 'localhost'],
 
-            ['port', null, InputOption::VALUE_OPTIONAL, 'The port to serve the application on', $config['port']],
+            ['port', null, InputOption::VALUE_OPTIONAL, 'The port to serve the application on', $config['port'] ?? ''],
 
             ['tries', null, InputOption::VALUE_OPTIONAL, 'The max number of ports to attempt to serve from', 10],
         ];
